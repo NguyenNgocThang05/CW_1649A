1011b7f431f7cb40e81ecd8c8edf6dc16a0063ef
Nguyen Ngoc Thang <thangnguyenngoc167@gmail.com>
2025-06-17 13:22:22 +0700

    commit

--- src/onlineBookStore/CustomArrayList.java
+++ src/onlineBookStore/CustomArrayList.java
@@ -2,66 +2,66 @@ package onlineBookStore;
 
 public class CustomArrayList<E> {
     // Attributes
-    private Object[] elements;     // array to store elements
-    private int nextIndex;
+    private Object[] data;     // array to store elements
+    private int size;       // size can also mean index
 
     // Constructor
     public CustomArrayList() {
-        elements = new Object[5]; // default array storage
-        nextIndex = 0;             // number of data has stored in the index
+        data = new Object[5]; // default array storage
+        size = 0;             // number of data has stored in the index
     }
 
     // Methods
     // Method 1: Add element at the end
     public void add(E value) {
-        if (nextIndex == elements.length) {
+        if (size == data.length) {
             resize();
         }
-        elements[nextIndex] = value;
-        nextIndex++;
+        data[size] = value;
+        size++;
     }
 
     // Method 2: Get element at index
     public E get(int index) {
-        if (index < 0 || index >= index) {
+        if (index < 0 || index >= size) {
             System.out.println("Index out of bounds");
             return null;
         }
-        return (E) elements[index];
+        return (E) data[index];
     }
 
     // Method 3: Remove element at index
     public void remove(int index) {
-        if (index < 0 || index >= index) {
+        if (index < 0 || index >= size) {
             System.out.println("Index out of bounds");
             return;
         }
-        for (int i = index; i < index - 1; i++) {
-            elements[i] = elements[i + 1]; // shift elements to left
+        for (int i = index; i < size - 1; i++) {
+            data[i] = data[i + 1]; // shift elements to left
         }
-        index--;
+        size--;
     }
 
     // Method 4: Get current size
     public int size() {
-        return nextIndex;
+        return size;
     }
 
     // Method 5: Resize the array when full
     private void resize() {
-        Object[] newData = new Object[elements.length * 2]; //double the size
-        for (int i = 0; i < elements.length; i++) {
-            newData[i] = elements[i]; //copy the old data to the new resized data
+        Object[] newData = new Object[data.length * 2]; //double the size
+        for (int i = 0; i < data.length; i++) {
+            newData[i] = data[i]; //copy the old data to the new resized data
         }
-        elements = newData;
+        data = newData;
     }
 
     // Method 6: Set element at index
     public void set(int index, E value) {
-        if (index < 0 || index >= index) {
+        if (index < 0 || index >= size) {
             System.out.println("Index out of bounds");
             return;
         }
-        elements[index] = value;
+        data[index] = value;
     }
 }
