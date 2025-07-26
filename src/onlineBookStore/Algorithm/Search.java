f0eb38532ec9b583d2eefbd034a480f97db694ca
Nguyen Ngoc Thang <thangnguyenngoc167@gmail.com>
2025-07-26 22:08:11 +0700

    commit

--- src/onlineBookStore/Algorithm/Search.java
+++ src/onlineBookStore/Algorithm/Search.java
@@ -6,23 +6,47 @@ import onlineBookStore.Model.Order;
 
 public class Search {
     public static Order searchOrderById(OrderList allOrders, int targetOrderID) {
-        LinkedQueueADT<Order> orders = allOrders.getAllOrders();
-        int size = orders.size();
+        LinkedQueueADT<Order> queue = allOrders.getAllOrders();
+        int size = queue.size();
 
-        for (int i = 0; i < size; i++) {
-            Order current = orders.poll();
-            if (current.getOrderID() == targetOrderID) {
-                // Put the order back in the queue (since we removed it)
-                orders.offer(current);
-                // Put remaining elements back
-                for (int j = i + 1; j < size; j++) {
-                    orders.offer(orders.poll());
-                }
-                return current;
+        // Binary search on the queue
+        int left = 0;
+        int right = size - 1;
+
+        while (left <= right) {
+            int mid = left + right / 2;
+
+            // Get the middle element by traversing from head
+            Order midOrder = getOrderAt(queue, mid);
+            int midOrderID = midOrder.getOrderID();
+
+            if (midOrderID == targetOrderID) {
+                return midOrder;
+            } else if (midOrderID < targetOrderID) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
-            // Put the order back in the queue
-            orders.offer(current);
         }
-        return null; // Not found
+        return null;
+    }
+
+    private static Order getOrderAt(LinkedQueueADT<Order> queue, int index) {
+        // Temporary queue to store dequeued elements
+        LinkedQueueADT<Order> tempQueue = new LinkedQueueADT<>();
+        Order result = null;
+
+        // Dequeue until we reach the desired index
+        for (int i = 0; i <= index; i++) {
+            result = queue.poll();
+            tempQueue.offer(result);
+        }
+
+        // Put all elements back in original queue
+        while (!tempQueue.isEmpty()) {
+            queue.offer(tempQueue.poll());
+        }
+
+        return result;
     }
 }
